587d4ab6827bc90537c5fd1191d610561c921912
Daniel Kilgallon <danielkilgallon2@gmail.com>
2022-01-07 19:01:58 -0500

    Removing armor tick

--- src/main/java/com/slick/randomthings/item/SuperLubricentBoots.java
+++ src/main/java/com/slick/randomthings/item/SuperLubricentBoots.java
@@ -2,11 +2,9 @@ package com.slick.randomthings.item;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 public class SuperLubricentBoots extends ArmorItem {
@@ -19,10 +17,5 @@ public class SuperLubricentBoots extends ArmorItem {
     @Override
     public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
         return "randomthings:textures/models/armor/super_lubricent_boots.png";
-    }
-
-    @Override
-    public void onArmorTick(ItemStack stack, Level world, Player player) {
-        player.setDiscardFriction(false);
     }
 }
