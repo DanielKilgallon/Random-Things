8bf6e2523230dab0ea7eb0447555017c3d7ccdec
Daniel Kilgallon <danielkilgallon2@gmail.com>
2022-02-03 16:11:13 -0500

    adding recipe, fixing bugs

---
+++ src/main/java/com/slick/randomthings/item/Ectoplasm.java
@@ -0,0 +1,9 @@
+package com.slick.randomthings.item;
+
+import net.minecraft.world.item.Item;
+
+public class Ectoplasm extends Item {
+    public Ecotoplasm(Properties properties) {
+        super(properties);
+    }
+}
